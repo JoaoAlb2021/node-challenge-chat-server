22fe775ffb083b16d82173c341962b0c557af675
JoaoAlb2021 <joaorenato2015@gmail.com>
2022-06-14 22:18:58 -0500

    Upload level 1 and level 2

--- server.js
+++ server.js
@@ -1,25 +1,72 @@
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
 
 const app = express();
+app.use(express.json())
 
 app.use(cors());
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+
+function getQuotesFromDatabase() {
+  const text = fs.readFileSync("messages.json");
+  return JSON.parse(text);
+}
+
+function saveQuotesToDatabase(arr) {
+  return fs.writeFileSync("messages.json", JSON.stringify(arr,null,2));
+}
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, () => {
-   console.log("Listening on port 3000")
+app.get("/messages", function (request, response) {
+  const messages = getQuotesFromDatabase()
+  response.send(messages)
+})
+
+app.post("/messages", function (request, response) {
+  const messages = getQuotesFromDatabase()
+  const obj = request.body
+  obj.id = messages.length
+  if (obj.text.trim().length > 0 && obj.from.trim().length > 0){
+    obj.text = obj.text.trim()
+    obj.from = obj.from.trim()
+    messages.push(obj)
+    saveQuotesToDatabase(messages)
+    response.send(obj)
+  }
+  else {
+    response.send("400 Bad Request....Name and Message are invalid.")
+  }
+
+})
+
+app.get('/messages/search', function(request,response) {
+  const messages = getQuotesFromDatabase()
+  const id = Number(request.query.id)
+  const messageId = messages.find(obj => obj.id === id)
+  response.send(messageId)
+})
+
+app.delete('/messages/delete/:id', function(request,response){
+  let messages = getQuotesFromDatabase()
+  const id = Number(request.params.id)
+  const messageId = messages.find(obj => obj.id === id)
+  messages = messages.filter(obj => obj.id !== id)
+  saveQuotesToDatabase(messages)
+  response.send(messageId)
+})
+
+const port = 3000
+app.listen(port, () => {
+   console.log("Listening on port: http://localhost:" + port);
   });
